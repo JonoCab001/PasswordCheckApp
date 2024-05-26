d7cfe0d613c465eccac1d6ad593a4010e5fe5d60
Jono Cabrera <cjonathan193@gmail.com>
2024-05-26 20:52:37 +1000

    Add project files.

---
+++ PasswordCheckApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCheckApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool uppercase = true;
+            bool symbols = false;
+
+            bool passwordValid = (uppercase && !symbols);
+
+            Console.WriteLine(passwordValid);
+            Console.ReadKey();
+        }
+    }
+}
